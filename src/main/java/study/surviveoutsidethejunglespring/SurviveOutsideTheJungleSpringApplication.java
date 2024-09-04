90ad9ed8abbd71526ebf3c6ae281bc19a562d61d
jun9898 <nice1998@gmail.com>
2024-09-04 18:30:37 +0900

    Init : project init

---
+++ src/main/java/study/surviveoutsidethejunglespring/SurviveOutsideTheJungleSpringApplication.java
@@ -0,0 +1,13 @@
+package study.surviveoutsidethejunglespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurviveOutsideTheJungleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurviveOutsideTheJungleSpringApplication.class, args);
+	}
+
+}
